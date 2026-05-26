29af9dcee61a21d9ee1d8bac1c6ba9f7f756c172
baileytorch <baileytorch@hotmail.com>
2026-05-26 16:59:15 -0400

    Validando Form

--- scripts/ap_n3_c1.js
+++ scripts/ap_n3_c1.js
@@ -8,5 +8,15 @@ window.onload = function () {
 function validarFormulario(){
     let nombre = document.getElementById('inputNombre');
     let email = document.getElementById('inputEmail');
-    console.log(nombre.value + '\n' + email.value)
+    let rut = document.getElementById('inputRut');
+    let telefono = document.getElementById('inputTelefono');
+    let contrasena = document.getElementById('inputContrasena');
+    let repContrasena = document.getElementById('inputRepetirContrasena');
+    let fechaNacimiento = document.getElementById('inputFechaNac');
+    let genero = document.querySelector('input[name="radioGenero"]:checked');
+    let pais = document.getElementById('selectPais');
+
+    if(nombre.value == ''){
+        nombre.classList.add('is-invalid');
+    }else{}
 }
