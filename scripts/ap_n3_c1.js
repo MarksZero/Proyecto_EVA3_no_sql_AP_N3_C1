c615665a9c857d717b3b6749f14c23701bfe608a
baileytorch <baileytorch@hotmail.com>
2026-05-28 16:12:20 -0400

    Validando Form

--- scripts/ap_n3_c1.js
+++ scripts/ap_n3_c1.js
@@ -1,11 +1,4 @@
-
-
-window.onload = function () {
-    let nombre = document.getElementById('inputNombre');
-    console.log(nombre.value)
-}
-
-function validarFormulario(){
+function validarFormulario() {
     let nombre = document.getElementById('inputNombre');
     let email = document.getElementById('inputEmail');
     let rut = document.getElementById('inputRut');
@@ -16,7 +9,68 @@ function validarFormulario(){
     let genero = document.querySelector('input[name="radioGenero"]:checked');
     let pais = document.getElementById('selectPais');
 
-    if(nombre.value == ''){
-        nombre.classList.add('is-invalid');
-    }else{}
+    if (nombre.value == '') {
+        nombre.classList.add('is-invalid', 'alerta');
+    } else {
+        nombre.classList.remove('is-invalid', 'alerta');
+        nombre.classList.add('is-valid');
+    }
+
+    if (email.value == '' || !validarEmail(email.value)) {
+        email.classList.add('is-invalid', 'alerta');
+    } else {
+        email.classList.remove('is-invalid', 'alerta');
+        email.classList.add('is-valid');
+    }
+
+    if (rut.value == '') {
+        rut.classList.add('is-invalid', 'alerta');
+    } else {
+        rut.classList.remove('is-invalid', 'alerta');
+        rut.classList.add('is-valid');
+    }
+
+    if (telefono.value == '') {
+        telefono.classList.add('is-invalid', 'alerta');
+    } else {
+        telefono.classList.remove('is-invalid', 'alerta');
+        telefono.classList.add('is-valid');
+    }
+
+    if (contrasena.value == '') {
+        contrasena.classList.add('is-invalid', 'alerta');
+    } else {
+        contrasena.classList.remove('is-invalid', 'alerta');
+        contrasena.classList.add('is-valid');
+    }
+
+    if (repContrasena.value == '') {
+        repContrasena.classList.add('is-invalid', 'alerta');
+    } else {
+        repContrasena.classList.remove('is-invalid', 'alerta');
+        repContrasena.classList.add('is-valid');
+    }
+
+    if (fechaNacimiento.value == '') {
+        fechaNacimiento.classList.add('is-invalid', 'alerta');
+    } else {
+        fechaNacimiento.classList.remove('is-invalid', 'alerta');
+        fechaNacimiento.classList.add('is-valid');
+    }
+
+    if (pais.value == '') {
+        pais.classList.add('is-invalid', 'alerta');
+    } else {
+        pais.classList.remove('is-invalid', 'alerta');
+        pais.classList.add('is-valid');
+    }
+}
+
+function validarEmail(email){
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexEmail.test(email);
+};
+
+function validarCongtrasena(contrasena){
+    
 }
