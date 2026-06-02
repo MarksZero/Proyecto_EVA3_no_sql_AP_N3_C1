1330aee189eafc0650345e2578751b5896c80905
baileytorch <baileytorch@hotmail.com>
2026-06-02 16:44:54 -0400

    Validando formulario

--- scripts/ap_n3_c1.js
+++ scripts/ap_n3_c1.js
@@ -8,61 +8,53 @@ function validarFormulario() {
     let fechaNacimiento = document.getElementById('inputFechaNac');
     let genero = document.querySelector('input[name="radioGenero"]:checked');
     let pais = document.getElementById('selectPais');
+    let formularioValido = true;
 
-    if (nombre.value == '') {
-        nombre.classList.add('is-invalid', 'alerta');
-    } else {
-        nombre.classList.remove('is-invalid', 'alerta');
-        nombre.classList.add('is-valid');
+    if (!validarCampo(nombre)) {
+        formularioValido = false;
     }
 
-    if (email.value == '' || !validarEmail(email.value)) {
-        email.classList.add('is-invalid', 'alerta');
-    } else {
-        email.classList.remove('is-invalid', 'alerta');
-        email.classList.add('is-valid');
+    if (!validarCampo(email) || !validarEmail(email.value)) {
+        formularioValido = false;
     }
 
-    if (rut.value == '') {
-        rut.classList.add('is-invalid', 'alerta');
-    } else {
-        rut.classList.remove('is-invalid', 'alerta');
-        rut.classList.add('is-valid');
+    if (!validarCampo(rut) || !validarRut(rut)) {
+        formularioValido = false;
     }
 
-    if (telefono.value == '') {
-        telefono.classList.add('is-invalid', 'alerta');
-    } else {
-        telefono.classList.remove('is-invalid', 'alerta');
-        telefono.classList.add('is-valid');
+    if (!validarCampo(telefono)) {
+        formularioValido = false
     }
 
-    if (contrasena.value == '') {
-        contrasena.classList.add('is-invalid', 'alerta');
-    } else {
-        contrasena.classList.remove('is-invalid', 'alerta');
-        contrasena.classList.add('is-valid');
+    if (!validarCampo(contrasena) || !validarContrasena(contrasena.value)) {
+        formularioValido = false
     }
 
-    if (repContrasena.value == '') {
-        repContrasena.classList.add('is-invalid', 'alerta');
-    } else {
-        repContrasena.classList.remove('is-invalid', 'alerta');
-        repContrasena.classList.add('is-valid');
+    if (!validarCampo(repContrasena) || repContrasena.value != contrasena.value) {
+        formularioValido = false
     }
 
-    if (fechaNacimiento.value == '') {
-        fechaNacimiento.classList.add('is-invalid', 'alerta');
-    } else {
-        fechaNacimiento.classList.remove('is-invalid', 'alerta');
-        fechaNacimiento.classList.add('is-valid');
+    if (!validarCampo(fechaNacimiento)) {
+        formularioValido = false
     }
 
-    if (pais.value == '') {
-        pais.classList.add('is-invalid', 'alerta');
+    if (!validarCampo(pais)) {
+        formularioValido = false
+    }
+
+    if(validarFormulario != false){
+        alert('Datos ingresados correctamente, enviado al servidor...')
+    }
+}
+
+function validarCampo(campo){
+    if (campo.value == '') {
+        campo.classList.add('is-invalid', 'alerta');
+        return false
     } else {
-        pais.classList.remove('is-invalid', 'alerta');
-        pais.classList.add('is-valid');
+        campo.classList.remove('is-invalid', 'alerta');
+        campo.classList.add('is-valid');
+        return true
     }
 }
 
@@ -71,6 +63,34 @@ function validarEmail(email){
     return regexEmail.test(email);
 };
 
-function validarCongtrasena(contrasena){
+function validarContrasena(contrasena){
+    const regexContrasena = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    return regexContrasena.test(contrasena);
+}
+
+function validarRut(rut) {
+    var valor = rut.value.replace('.','');
+    valor = valor.replace('-','');
     
+    cuerpo = valor.slice(0,-1);
+    dv = valor.slice(-1).toUpperCase();
+    rut.value = cuerpo + '-'+ dv
+    
+    if(cuerpo.length < 7) { return false;}
+    suma = 0;
+    multiplo = 2;
+    
+    for(i=1;i<=cuerpo.length;i++) {
+        index = multiplo * valor.charAt(cuerpo.length - i);
+        suma = suma + index;        
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }  
+    }
+    
+    dvEsperado = 11 - (suma % 11);
+
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 0)?11:dv;
+
+    if(dvEsperado != dv) { return false; }
+    return true;
 }
